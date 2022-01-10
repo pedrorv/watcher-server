4ecea176da60f9ed3dcf79368711aa8db5fe3361
Pedro Reis <pedroreisv@gmail.com>
2022-01-10 08:50:05 -0300

    Fix cors errors

--- src/main.ts
+++ src/main.ts
@@ -7,22 +7,26 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(cors());
+app.options('*', cors());
 app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/sessions', async (_, res) => {
-  const sessionIds = await PgClient.query(`select distinct(session_id) from events`);
-
-  res.json(sessionIds);
+  try {
+    const sessionIds = await PgClient.query(`select distinct(session_id) from events`);
+    res.json(sessionIds.map(({ session_id }) => session_id));
+  } catch (e) {
+    res.json({ error: true, message: e.message });
+  }
 });
 
 app.post('/events', async (req, res) => {
-  const events = req.body as WatcherEvent[];
-  const insertQueries = events.map(({ type, name, path, timestamp, sessionId, properties }) => ({
-    query: `insert into events (type, name, path, timestamp, session_id, properties) values ($1, $2, $3, $4, $5, $6)`,
-    args: [type, name, path, timestamp, sessionId, properties],
-  }));
-
   try {
+    const events = req.body as WatcherEvent[];
+    const insertQueries = events.map(({ type, name, path, timestamp, sessionId, properties }) => ({
+      query: `insert into events (type, name, path, timestamp, session_id, properties) values ($1, $2, $3, $4, $5, $6)`,
+      args: [type, name, path, timestamp, sessionId, properties],
+    }));
     await PgClient.transaction(insertQueries);
     res.json({ error: false, message: 'Events inserted' });
   } catch (e) {
