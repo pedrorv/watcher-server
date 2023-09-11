b03479d1273a75739faeffcbbdd8b2895321abe2
Pedro Reis <pedroreisv@gmail.com>
2023-09-10 23:11:25 -0300

    fix: lint

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-import fs from 'fs';
 import express from 'express';
 import cors from 'cors';
 import { PgClient } from './pg-client';
@@ -10,7 +9,7 @@ const app = express();
 
 app.use(cors());
 app.options('*', cors());
-app.use(express.json({ limit: '6mb' }));
+app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(gzip);
 
